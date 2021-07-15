3a7037411835c85c68825e48d6abb6c6534c6727
romanklopyk <86305348+romanklopyk@users.noreply.github.com>
2021-07-15 19:50:30 +0300

    класна робота 15,07,2021

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
+// опрацьовуємо подію запуску слайдера // всього слайдів
+$('.slider1').on('init', function(event, slick) {
+    console.log(slick.slideCount);
+    var T = slick.slideCount;
+    $("#total_slides").html(T);
+});
+
+// On before slide change
+$('.slider1').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+    var C = slick.currentSlide;
+    $("#current_slide").html(C + 1);
+});
+
 $(document).ready(function() {
-    $('.slider1').slick({
-        autoplay: true
-       
-    });
+    $('.slider1').slick();
+});
+
+$('#slider_next').on('click', function() {
+    $('.slider1').slick('slickNext');
+});
+
+$('#slider_prev').on('click', function() {
+    $('.slider1').slick('slickPrev');
 });
