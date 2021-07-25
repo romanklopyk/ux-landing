a6d65e39b6c5f87f8773204f1d19f078c25fa6de
romanklopyk <rx@lyc2.org>
2021-07-25 19:41:58 +0300

    завершив сторінку
    без адаптвного дизайну, згруба

--- js/app.js
+++ js/app.js
@@ -22,3 +22,37 @@ $('#slider_next').on('click', function() {
 $('#slider_prev').on('click', function() {
     $('.slider1').slick('slickPrev');
 });
+
+// слайдер домашнє завдання
+
+
+$(document).ready(function() {
+    $('.slider2').slick({
+        autoplay: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+});
+
+// опрацьовуємо подію запуску слайдера // всього слайдів // слайдер 2!!!
+$('.slider2').on('init', function(event, slick) {
+    console.log(slick.slideCount);
+    var T1 = slick.slideCount;
+    $("#tot_slides").html(T1);
+});
+
+// On before slide change
+$('.slider2').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+    var C = slick.currentSlide;
+    $("#cur_slide").html(C + 1);
+});
+
+$('#sl_next').on('click', function() {
+    $('.slider2').slick('slickNext');
+});
+
+$('#sl_prev').on('click', function() {
+    $('.slider2').slick('slickPrev');
+});
